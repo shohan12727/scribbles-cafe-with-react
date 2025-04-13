3afc40b36cd664690af84bce1da6972a30e5f592
shohan12727 <aishohan001@gmail.com>
2025-04-13 16:35:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+// import Navbar from './component/navbar/Navbar';
+function App() {
+
+  return (
+    <>
+      {/* <Navbar></Navbar> */}
+       <h1>hello world </h1>
+    </>
+  )
+}
+
+export default App
